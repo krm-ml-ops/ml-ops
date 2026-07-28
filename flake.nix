584452a7a55878c3b44beafac368503f678c5fc1
Danila Danko <br4ch1st0chr0n3@gmail.com>
2026-07-28 09:57:13 +0300

    feat(nix): add pandoc to the devshell

--- flake.nix
+++ flake.nix
@@ -17,6 +17,7 @@
               pkgs.uv
               pkgs.git
               pkgs.nodejs_22
+              pkgs.pandoc
             ];
             LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc ];
           };
